df94d547cf9e49327a87290b42454d06863b7db0
ppyczko <ppyczko@apsl.net>
2024-03-19 13:00:31 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,8 +103,6 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Madrid'
 
-STATIC_ROOT = BASE_DIR / 'static'
-
 USE_I18N = True
 
 USE_L10N = True
@@ -115,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
