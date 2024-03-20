18a8e14b58ed7020872b77a45d035d71c71e4209
ppyczko <ppyczko@apsl.net>
2024-03-20 15:37:54 +0100

    Ej 2.1 Formularios con estilo DjangoOOPs

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,24 @@
 from django import forms
+from crispy_forms.helper import FormHelper, Layout
+from crispy_forms.layout import Submit, Row, Column, Field, Button
 
 from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+
+        # If you pass FormHelper constructor a form instance
+        # It builds a default layout with all its fields
+        self.helper = FormHelper(self)
+
+        # You can dynamically adjust your layout
+        self.helper.layout = Layout(
+            Row(Column(Field("title", autofocus=True))),
+            Row(Column("text")),
+            Submit("submit", "Save", css_class="btn btn-secondary btn-lg w-100"),
+        )
 
     class Meta:
         model = Post
