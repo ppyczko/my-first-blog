e39d9dcafb4cd96c8b10e072dcbd137c2648015f
ppyczko <ppyczko@apsl.net>
2024-03-20 10:30:26 +0100

    Ej 1.0 Blog DjangoOOPs

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,47 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 from .forms import PostForm
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
-        "published_date"
-    )
-    return render(request, "blog/post_list.html", {"posts": posts})
+class PostListView(ListView):
+    model = Post
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by(
+            "published_date"
+        )
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        return context
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, "blog/post_detail.html", {"post": post})
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+#         "published_date"
+#     )
+#     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        print(self)
+        return context
+
+
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, "blog/post_detail.html", {"post": post})
 
 
 def post_new(request):
